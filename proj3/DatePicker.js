e65599714e2e105bf06af6c5ce9c78286d8a37c8
ReedLeo <hireedleo@gmail.com>
2023-02-11 00:43:34 +0800

    finished the proj3 problem1, build a calendar

--- proj3/DatePicker.js
+++ proj3/DatePicker.js
@@ -1,163 +1,222 @@
 'use strict';
 
 class DatePicker {
-    static daysInMonths = [
-        [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31], // common year
-        [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] // leap year
-    ];
-
-    static monthsName = [
-        "January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ];
-
     constructor(id, callback) {
         this.id = id;
         this.callback = callback;
     }
 
-    isLeapYear(year) {
+    static getMonthName(month) {
+        const monthsName = [
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        return monthsName[month];
+    }
+
+    static isLeapYear(year) {
         return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
     }
 
-    getPrefixDays(date) {
-        let firstDayOfCurMonth = new Date(date);
-        firstDayOfCurMonth.setDate(1);
-        const dayInWeek = firstDayOfCurMonth.getDay(); // 0-6
-        const prevMonth = (date.getMonth() > 0) ? (date.getMonth() - 1) : 11;
-        const isLeap = this.isLeapYear(date.getFullYear() - (prevMonth === 11 ? 1 : 0));
-        const prevMonthTotalDays = DatePicker.daysInMonths[isLeap ? 1 : 0][prevMonth];
-
-        let prefixDays = [];
-        for (let i = 0; i < dayInWeek; ++i) {
-            prefixDays.push(prevMonthTotalDays - dayInWeek + 1 + i);
-        }
-        return prefixDays;
+    static getDaysOfMonth(year, month) {
+        const daysInMonths = [
+            [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31], // common year
+            [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] // leap year
+        ];
+        const isLeap = DatePicker.isLeapYear(year) ? 1 : 0;
+        return daysInMonths[isLeap][month];
     }
 
-    getPostfixDays(date) {
-        let lastDayOfCurMonth = new Date(date);
-        const month = lastDayOfCurMonth.getMonth();
-        const isCurLeap = this.isLeapYear(date.getFullYear());
-        const curMonthTotalDays = DatePicker.daysInMonths[isCurLeap ? 1 : 0][month];
-        lastDayOfCurMonth.setDate(curMonthTotalDays);
-        const dayInWeek = lastDayOfCurMonth.getDay();
-
-        let postfixDays = [];
-        for (let i = dayInWeek + 1; i < 7; ++i) {
-            postfixDays.push(i - dayInWeek);
+    static getPrevOrNextMonDays(date, type) {
+        const curYear = date.getFullYear();
+        const curMonth = date.getMonth();
+        if (type === 'prev') {
+            const prevMonth = (curMonth === 0) ? 11 : (curMonth - 1);
+            const prevYear = (prevMonth === 11) ? (curYear - 1) : curYear;
+            return {
+                year: prevYear,
+                month: prevMonth,
+                days: DatePicker.getDaysOfMonth(prevYear, prevMonth)
+            };
+        } else {
+            const nextMonth = (curMonth === 11) ? 0 : (curMonth + 1);
+            const nextYear = (nextMonth === 0) ? (curYear + 1) : curYear;
+            return {
+                year: nextYear,
+                month: nextMonth,
+                days: DatePicker.getDaysOfMonth(nextYear, nextMonth)
+            };
         }
-        return postfixDays;
     }
 
-    renderCalendarHeader(target, date) {
+    static generateCalendar(date) {
+        const curYear = date.getFullYear();
+        const curMonth = date.getMonth(); // 0-11
+        const curMonDays = DatePicker.getDaysOfMonth(curYear, curMonth);
+        const { year: prevYear, month: prevMon, days: prevMonDays } = DatePicker.getPrevOrNextMonDays(date, 'prev');
+        const { year: nextYear, month: nextMon } = DatePicker.getPrevOrNextMonDays(date, 'next');
+        const fisrtDayInWeekOfCurMon = new Date(`${curYear}/${curMonth + 1}/1`).getDay();
+        const lastDayInWeekOfCurMon = new Date(`${curYear}/${curMonth + 1}/${curMonDays}`).getDay();
 
+        // number of previous month's days to fill.
+        const numOfPrefixDays = fisrtDayInWeekOfCurMon;
+
+        // number of next month's days to fill.
+        const numOfPostfixDays = 6 - lastDayInWeekOfCurMon;
+
+        const fullCalendar = [];
+
+        // fill the previous month's days.
+        for (let i = 0; i < numOfPrefixDays; ++i) {
+            fullCalendar.push({
+                year: prevYear,
+                month: prevMon,
+                day: prevMonDays - numOfPrefixDays + 1 + i,
+                isCurMon: false
+            });
+        }
+
+        // fill the current month's days.
+        for (let i = 1; i <= curMonDays; ++i) {
+            fullCalendar.push({
+                year: curYear,
+                month: curMonth,
+                day: i,
+                isCurMon: true
+            });
+        }
+
+        // fill the next month's days.
+        for (let i = 1; i <= numOfPostfixDays; ++i) {
+            fullCalendar.push({
+                year: nextYear,
+                month: nextMon,
+                day: i,
+                isCurMon: false
+            });
+        }
+
+        return fullCalendar;
+    }
+
+    static renderCalendarHeader(target, date) {
         const year = date.getFullYear();
         const monthIdx = date.getMonth(); // 0-11
-        target.textContent = `${DatePicker.monthsName[monthIdx]} ${year}`;
+        target.textContent = `${DatePicker.getMonthName(monthIdx)} ${year}`;
     }
 
     renderCalendarTable(target, date) {
+        const curYear = date.getFullYear();
+        const curMonth = date.getMonth();
+        const curDate = date.getDate();
 
-        const leapYear = this.isLeapYear(date.getFullYear());
+        const frag = document.createDocumentFragment();
+        this.fullCalenderDays = DatePicker.generateCalendar(date);
+        target.textContent = "";
+        this.fullCalenderDays.forEach((dayInfo) => {
+            const isActive = [
+                dayInfo.year === curYear,
+                dayInfo.month === curMonth,
+                dayInfo.day === curDate,
+                dayInfo.isCurMon
+            ].every(Boolean);
 
-        const month = date.getMonth(); // 0-11
-        const totalDaysOfCurMonth = DatePicker.daysInMonths[leapYear ? 1 : 0][month];
-
-        const dayInMonth = date.getDate(); // 1-31
-
-        const prefixDays = this.getPrefixDays(date);
-        const postfixDays = this.getPostfixDays(date);
-
-        const calFragment = document.createDocumentFragment();
-        let curDay = 1 - prefixDays.length;
-        if (prefixDays !== null && prefixDays.length > 0) {
-            for (const x of prefixDays) {
-                const li = document.createElement("LI");
-                li.className = "date flex-center prev-month-days light";
-
-                const div = document.createElement("DIV");
-                div.className = "date-num flex-center";
-                div.innerHTML = x;
-                li.appendChild(div);
-                calFragment.appendChild(li);
-                ++curDay;
+            const li = document.createElement("li");
+            const div = document.createElement("div");
+            li.className = `date flex-center${dayInfo.isCurMon ? '' : ' light'}`;
+            if (isActive) {
+                this.activeCell = li;
+                li.className += ' active';
             }
+            div.className = 'date-num flex-center';
+            div.textContent = dayInfo.day;
+            li.appendChild(div);
+            frag.appendChild(li);
+        });
 
-            for (let i = prefixDays.length; i < 7; ++i) {
-                const li = document.createElement("LI");
-                li.className = "date flex-center cur-month-days";
-                if (curDay === dayInMonth) {
-                    li.className += " active";
-                }
-                li.id = `cur-month-day-${curDay}`;
-                
-                const div = document.createElement("DIV");
-                div.className = "date-num flex-center";
-                div.innerHTML = curDay;
-                li.appendChild(div);
-                calFragment.appendChild(li);
-                ++curDay;
+        target.appendChild(frag);
+    }
+
+    renderCalendar(date) {
+        const calHeader = document.getElementById(`cal-header-${this.id}`);
+        DatePicker.renderCalendarHeader(calHeader, date);
+
+        const calTable = document.getElementById(`content-${this.id}`);
+        this.renderCalendarTable(calTable, date);
+        
+        Array.from(calTable.children).forEach((singleDay) => {
+            if (singleDay.classList.contains("light") === false) {
+                singleDay.addEventListener('click', () => {
+                    this.callback(this.id, { 
+                        month: date.getMonth() + 1, 
+                        day: singleDay.textContent, 
+                        year: date.getFullYear() 
+                    });
+                    if (singleDay !== this.activeCell) {
+                        singleDay.className += ' active';
+                        this.activeCell.classList.remove('active');
+                        this.activeCell = singleDay;
+                    }
+                });
             }
+        });
+    }
+
+    changeMonth(type) {
+        const curYear = this.date.getFullYear();
+        const curMonth = this.date.getMonth();
+        let newYear = 1970;
+        let newMonth = 0;
+
+        if (type === 'prev') {
+            newMonth = (curMonth === 0) ? 11 : (curMonth - 1);
+            newYear = (newMonth === 11) ? (curYear - 1) : curYear;
+        } else {
+            newMonth = (curMonth === 11) ? 0 : (curMonth + 1);
+            newYear = (newMonth === 0) ? (curYear + 1) : curYear;
         }
 
-        for (let row = 1; curDay <= totalDaysOfCurMonth; ++row) {
-            for (let col = 0; (col < 7) && (curDay <= totalDaysOfCurMonth); ++col) {
-                const li = window.document.createElement("LI");
-                li.className = "date flex-center cur-month-days";
-                if (curDay === dayInMonth) {
-                    li.className += " active";
-                }
-                li.id = `cur-month-day-${curDay}`;
-                
-                const div = document.createElement("DIV");
-                div.className = "date-num flex-center";
-                div.innerHTML = curDay;
-                li.appendChild(div);
-                calFragment.appendChild(li);
-                ++curDay;
-            }
-
-            // add the next months' days.
-            if (curDay > totalDaysOfCurMonth) {
-                for (let x of postfixDays) {
-                    let li = window.document.createElement("LI");
-                    li.className = "date flex-center nxt-month-days light";
-                    li.id = `nxt-month-day-${x}`;
-
-                    const div = document.createElement("DIV");
-                    div.className = "date-num flex-center";
-                    div.innerHTML = x;
-                    li.appendChild(div);
-                    calFragment.appendChild(li);
-                    ++curDay;
-                }
-            }
-        }
-        target.appendChild(calFragment);
+        this.date = new Date(`${newYear}/${newMonth + 1}/1`);
+        this.render(this.date);
     }
 
     /* 
         1. The calendar must display the days of the selected month in a grid with one line
            for each week and one column for each day of the week.
+
         2. Weeks run from Sunday on the left to Saturday on the right. The calendar must 
-           contain a header row displaying abbreviations for the days of the week, such as "Su", "Mo", etc.
+           contain a header row displaying abbreviations for the days of the week, 
+           such as "Su", "Mo", etc.
+
         3. Each day of the month is displayed as a number.
-        4. Some weeks when displayed in the date picker may contain days not in the selected month. 
-           These days should be displayed as the number in their respective month, but in a dimmed fashion to indicate they are not part of the current month.
-        5. All weeks displayed should contain at least one day belonging to the current month. 
-           Most months will display 5 weeks, but some months may display 4 or 6 depending on the days. T
-           he number of rows in your calendar should not be fixed.
+
+        4. Some weeks when displayed in the date picker may contain days not in 
+           the selected month. These days should be displayed as the number in 
+           their respective month, but in a dimmed fashion to indicate they are 
+           not part of the current month.
+
+        5. All weeks displayed should contain at least one day belonging to the current 
+           month. Most months will display 5 weeks, but some months may display 4 or 6 
+           depending on the days. The number of rows in your calendar should not be fixed.
+
         6. The calendar must display the name of the month and year at the top of the calendar. 
-           In addition, it must display controls such as "<" and ">" that can be clicked to change the calendar's display to the previous or next month.
-        7. Clicking on a valid day of the current month should invoke the callback specified on the 
-           constructor with the arguments described above. Clicking on days belonging to months other 
-           than the current month should not invoke the callback.
+           In addition, it must display controls such as "<" and ">" that can be clicked to change 
+           the calendar's display to the previous or next month.
+
+        7. Clicking on a valid day of the current month should invoke the callback specified 
+           on the constructor with the arguments described above. Clicking on days belonging 
+           to months other than the current month should not invoke the callback.
     */
     render(date) {
-        this.callback(this.id, { month: date.getMonth() + 1, day: date.getDate(), year: date.getFullYear() });
-        let datePicker = window.document.getElementById(this.id);
+        this.date = date;
 
+        this.callback(this.id, { 
+            month: date.getMonth() + 1, 
+            day: date.getDate(), 
+            year: date.getFullYear() 
+        });
+
+        const datePicker = document.getElementById(this.id);
         datePicker.innerHTML = `
             <div class="calendar" id="calendar-${this.id}">
                 <div class="title" id="title-${this.id}">
@@ -180,11 +239,12 @@ class DatePicker {
             </div>
         `;
 
-        const calHeader = window.document.getElementById(`cal-header-${this.id}`);
-        this.renderCalendarHeader(calHeader, date);
-
-        const calTable = window.document.getElementById(`content-${this.id}`);
-        this.renderCalendarTable(calTable, date);
-
+        this.renderCalendar(date);
+        document.getElementById(`prev-${this.id}`).addEventListener('click', () => {
+            this.changeMonth('prev');
+        });
+        document.getElementById(`next-${this.id}`).addEventListener('click', () => {
+            this.changeMonth('next');
+        });
     }
 }
